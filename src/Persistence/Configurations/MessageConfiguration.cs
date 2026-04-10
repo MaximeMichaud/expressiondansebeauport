6605b6cf193f4d396801b17de1f51f339dc0fb82
Alexandre Roy <alexandre15roy@gmail.com>
2026-04-10 14:07:12 -0400

    feat(persistence): configure new media columns on Message and PostMedia

--- src/Persistence/Configurations/MessageConfiguration.cs
+++ src/Persistence/Configurations/MessageConfiguration.cs
@@ -11,6 +11,8 @@ public class MessageConfiguration : IEntityTypeConfiguration<Message>
         builder.HasKey(m => m.Id);
         builder.Property(m => m.Content).HasMaxLength(5000).IsRequired();
         builder.Property(m => m.MediaUrl).HasMaxLength(500);
+        builder.Property(m => m.MediaThumbnailUrl).HasMaxLength(500);
+        builder.Property(m => m.MediaOriginalUrl).HasMaxLength(500);
 
         builder.HasOne(m => m.SenderMember)
             .WithMany()
