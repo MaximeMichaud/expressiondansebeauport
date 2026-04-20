860ef757e65df6f33bd6e51cd7944eaffe716881
Maxime Michaud <14856114+MaximeMichaud@users.noreply.github.com>
2026-04-20 14:39:33 -0400

    fix(cookie-banner): afficher uniquement sur le site public (#121)

--- src/Web/vue-app/src/components/layouts/PublicLayout.vue
+++ src/Web/vue-app/src/components/layouts/PublicLayout.vue
@@ -14,11 +14,13 @@
       </RouterView>
     </main>
     <PublicFooter />
+    <CookieBanner />
   </div>
 </template>
 
 <script lang="ts" setup>
 import PublicNavbar from "@/components/navigation/PublicNavbar.vue";
 import PublicFooter from "@/components/layouts/items/PublicFooter.vue";
+import CookieBanner from "@/components/layouts/items/CookieBanner.vue";
 import Loader from "@/components/layouts/items/Loader.vue";
 </script>
